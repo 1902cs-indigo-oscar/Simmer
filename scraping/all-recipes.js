6ca491cc34bc244fad910af1647dd9bfc118eb4c
Brandon Rowe <brandonhrowe@gmail.com>
2019-05-01 15:38:50 -0400

    scrapers exporting

--- scraping/all-recipes.js
+++ scraping/all-recipes.js
@@ -1,39 +1,54 @@
 const rp = require("request-promise");
 const $ = require("cheerio");
-const url =
-  "https://www.allrecipes.com/recipe/213656/amazing-crusted-chicken/?clickId=right%20rail0&internalSource=rr_feed_recipe_sb&referringId=272457%20referringContentType%3Drecipe";
+// const url =
+//   "https://www.allrecipes.com/recipe/213656/amazing-crusted-chicken/?clickId=right%20rail0&internalSource=rr_feed_recipe_sb&referringId=272457%20referringContentType%3Drecipe";
 
-const article = {
-  site: "All Recipes"
+const allrecipesScraper = url => {
+  return rp(url)
+    .then(function(html) {
+      let article = {
+        site: "All Recipes"
+      };
+      article.title = $(".recipe-summary__h1", html).text();
+      article.author = $(".submitter__name", html).text();
+      article.ingredients = $(".checkList__line", html)
+        .text()
+        .trim()
+        .split(/\s\s+/)
+        .filter(ingred => ingred !== "Add all ingredients to list");
+      let times = $(".prepTime__item", html)
+        .text()
+        .trim()
+        .split(/\n\s\s+/);
+      let nutr = $(".nutrition-summary-facts", html)
+        .text()
+        .trim()
+        .replace(/\s\s/g, " ")
+        .split(/;\s+|.?\s\s+/);
+      let nutrition = nutr.slice(1, nutr.length - 1);
+      article.misc = times.concat(nutrition);
+      article.instructions = $(".recipe-directions__list > li", html)
+        .text()
+        .trim()
+        .split(/\s\s+/);
+      article.imageUrl = $(".rec-photo", html).attr("src");
+      article.tags = $(".breadcrumbs", html)
+        .text()
+        .trim()
+        .split(/\n\s+/)
+        .filter(tag => tag !== "Home" && tag !== "Recipes");
+      return article;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 };
 
-rp(url)
-  .then(function(html) {
-    article.title = $(".recipe-summary__h1", html).text();
-    article.author = $(".submitter__name", html).text();
-    article.ingredients = $(".checkList__line", html)
-      .text()
-      .trim()
-      .split(/\s\s+/)
-      .filter(ingred => ingred !== "Add all ingredients to list");
-    article.times = $(".prepTime__item", html)
-      .text()
-      .trim()
-      .split(/\n\s\s+/);
-    article.instructions = $(".recipe-directions__list > li", html)
-      .text()
-      .trim()
-      .split(/\s\s+/);
-    let nutr = $(".nutrition-summary-facts", html)
-      .text()
-      .trim()
-      .replace(/\s\s/g, " ")
-      .split(/;\s+|.?\s\s+/);
-    article.nutrition = nutr.slice(1, nutr.length - 1);
-    article.imageUrl = $(".rec-photo", html).attr("src");
-    article.tags = $(".breadcrumbs", html).text().trim().split(/\n\s+/).filter(tag => tag !== "Home" && tag !== "Recipes")
-    console.log("article: ", article);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+module.exports = allrecipesScraper;
+
+// const scrape = async () => {
+//   let func = await allrecipesScraper(url)
+//   console.log(func)
+// }
+
+// scrape()
--- scraping/chowhound.js
+++ scraping/chowhound.js
@@ -2,56 +2,66 @@ const rp = require("request-promise");
 const $ = require("cheerio");
 const url = "https://www.chowhound.com/recipes/shrimp-po-boy-sandwich-32111";
 
-const article = {
-  site: "Chowhound"
+const chowhoundScraper = url => {
+  return rp(url)
+    .then(function(html) {
+      let article = {
+        site: "Chowhound"
+      };
+      article.title = $(".fr_r_info h1", html).text();
+      article.ingredients = $(".freyja_box ul", html)
+        .text()
+        .trim()
+        .split(/\n+/);
+      let info = [];
+      info.push($(".frr_serves", html).text());
+      info.push($(".frr_difficulty", html).text());
+      let times = $(".frr_totaltime", html)
+        .text()
+        .split(/\n/)
+        .map(data => data.trim())
+        .filter(data => data.length > 0);
+      if (times.length) {
+        times.splice(0, 1);
+        times.splice(1, 1);
+        times[0] = `Total: ${times[0]}`;
+        times[1] = `Active: ${times[1]}`;
+        info = info.concat(times);
+      }
+      article.misc = info;
+      let instructionsAndAuthor = $(".fr_instruction_rec .frr_wrap", html)
+        .text()
+        .trim()
+        .split(/\s\s+/);
+      article.instructions = instructionsAndAuthor
+        .slice(0, instructionsAndAuthor.length - 1)
+        .map(step => {
+          if (parseInt(step[0]) > 0) {
+            return step.slice(1);
+          } else {
+            return step;
+          }
+        });
+      article.author = instructionsAndAuthor
+        .slice(instructionsAndAuthor.length - 1)
+        .toString();
+      article.imageUrl = $(".fr_hdimgov img", html).attr("data-src");
+      let tags = $(".freyja_fulltags", html)
+        .text()
+        .split(/\n\s+/);
+      article.tags = tags.slice(1, tags.length - 1);
+      return article;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 };
 
-rp(url)
-  .then(function(html) {
-    article.title = $(".fr_r_info h1", html).text();
-    article.ingredients = $(".freyja_box ul", html)
-      .text()
-      .trim()
-      .split(/\n+/);
-    let info = [];
-    info.push($(".frr_serves", html).text());
-    info.push($(".frr_difficulty", html).text());
-    let times = $(".frr_totaltime", html)
-      .text()
-      .split(/\n/)
-      .map(data => data.trim())
-      .filter(data => data.length > 0);
-    if (times.length) {
-      times.splice(0, 1);
-      times.splice(1, 1);
-      times[0] = `Total: ${times[0]}`;
-      times[1] = `Active: ${times[1]}`;
-      info = info.concat(times);
-    }
-    article.times = info;
-    let instructionsAndAuthor = $(".fr_instruction_rec .frr_wrap", html)
-      .text()
-      .trim()
-      .split(/\s\s+/);
-    article.instructions = instructionsAndAuthor
-      .slice(0, instructionsAndAuthor.length - 1)
-      .map(step => {
-        if (parseInt(step[0]) > 0) {
-          return step.slice(1);
-        } else {
-          return step;
-        }
-      });
-    article.author = instructionsAndAuthor
-      .slice(instructionsAndAuthor.length - 1)
-      .toString();
-    article.imageUrl = $(".fr_hdimgov img", html).attr("data-src");
-    let tags = $(".freyja_fulltags", html)
-      .text()
-      .split(/\n\s+/);
-    article.tags = tags.slice(1, tags.length - 1);
-    console.log("article: ", article);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+module.exports = chowhoundScraper;
+
+// const scrape = async () => {
+//   let func = await chowhoundScraper(url)
+//   console.log(func)
+// }
+
+// scrape()
