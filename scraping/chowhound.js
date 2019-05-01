f14095db8945b8b0936790ac9c21b0ad4db12fa3
Brandon Rowe <brandonhrowe@gmail.com>
2019-05-01 11:26:55 -0400

    chowhound scrape

--- scraping/chowhound.js
+++ scraping/chowhound.js
@@ -1,6 +1,7 @@
 const rp = require("request-promise");
 const $ = require("cheerio");
-const url = "https://www.chowhound.com/recipes/creamy-beet-linguine-walnuts-feta-32095";
+const url =
+  "https://www.chowhound.com/recipes/creamy-beet-linguine-walnuts-feta-32095";
 
 const article = {
   site: "Chowhound"
@@ -10,23 +11,29 @@ rp(url)
   .then(function(html) {
     article.title = $(".fr_r_info h1", html).text();
     // article.author = $(".Post__author", html).text();
-    article.ingredients = $(".freyja_box freyja_box81 > li", html)
+    article.ingredients = $(".freyja_box ul", html)
       .text()
       .trim()
-      .split(/\s\s+/);
+      .split(/\n+/);
     let info = [];
-    article.info = $(".no_sep  frr_serves fr_sep", html).text();
-    article.instructions = $(".Recipe__instructions > li", html)
+    info.push($(".frr_serves", html).text());
+    info.push($(".frr_difficulty", html).text());
+    let times = $(".frr_totaltime", html)
+      .text()
+      .split(/\n/)
+      .map(data => data.trim())
+      .filter(data => data.length > 0);
+    times.splice(0, 1);
+    times.splice(1, 1);
+    times[0] = `Total: ${times[0]}`;
+    times[1] = `Active: ${times[1]}`;
+    info = info.concat(times);
+    article.info = info;
+    article.instructions = $(".fr_instruction_rec .frr_wrap", html)
       .text()
       .trim()
-      .split(/\s\s+/);
-    let nutr = $(".nutrition-summary-facts", html)
-      .text()
-      .trim()
-      .replace(/\s\s/g, " ")
-      .split(/;\s+|.?\s\s+/);
-    article.nutrition = nutr.slice(1, nutr.length - 1);
-    article.imageUrl = $(".Figure__image img", html).attr("src");
+      .split(/\s\s+/)
+    article.imageUrl = $(".fr_hdimgov img", html).attr("data-src");
     console.log("article: ", article);
   })
   .catch(function(err) {
