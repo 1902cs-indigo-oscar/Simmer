e8d1d391c4ed246aa98ce44561f9a1b1f53afed6
Brandon Rowe <brandonhrowe@gmail.com>
2019-05-02 18:10:35 -0400

    test field to add articles working

--- server/api/articles.js
+++ server/api/articles.js
@@ -1,29 +1,33 @@
-const router = require('express').Router();
-const { Article } = require('../db/models');
-const scraperObj = require('../scraping')
+const router = require("express").Router();
+const { Article } = require("../db/models");
+const scraperObj = require("../scraping");
 module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    console.log('Hit Me');
+    console.log("Hit Me");
     if (req.user) {
       const articles = await Article.findAll({
-        where: { userId: req.user.id },
+        where: { userId: req.user.id }
       });
       res.json(articles);
+    } else {
+      res.sendStatus(404);
     }
-    res.sendStatus(404);
   } catch (err) {
     next(err);
   }
 });
 
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     if (req.user) {
-      let urlTail = req.body.url.split('www.')[1]
-      const urlBase = urlTail.split('.com')[0]
-      const newArticle = scraperObj[urlBase]
+      console.log(req.body.url);
+      let urlTail = req.body.url.split("www.")[1];
+      const urlBase = urlTail.split(".com")[0];
+      console.log(urlBase);
+      const newArticle = await scraperObj[urlBase](req.body.url);
+      console.log("newArticle", newArticle);
       // const newArticle = {
       //   url: req.body.url,
       //   site: req.body.site,
@@ -38,38 +42,41 @@ router.post('/', async (req, res, next) => {
       // };
       const createdArticle = await Article.create(newArticle);
       res.json(createdArticle);
+    } else {
+      res.sendStatus(404);
     }
-    res.sendStatus(404);
   } catch (err) {
     next(err);
   }
 });
 
-router.get('/:articleId', async (req, res, next) => {
+router.get("/:articleId", async (req, res, next) => {
   try {
     if (req.user) {
       const id = Number(req.params.articleId);
       const article = await Article.findByPk(id);
       res.json(article);
+    } else {
+      res.sendStatus(404);
     }
-    res.sendStatus(404);
   } catch (err) {
     next(err);
   }
 });
 
-router.delete('/:articleId', async (req, res, next) => {
+router.delete("/:articleId", async (req, res, next) => {
   try {
     if (req.user) {
       await Article.destroy({
         where: {
           id: req.params.articleId,
-          userId: req.user.id,
-        },
+          userId: req.user.id
+        }
       });
       res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
     }
-    res.sendStatus(404);
   } catch (err) {
     next(err);
   }
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,24 +1,40 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {createNewArticle} from '../store';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createNewArticle } from "../store";
 
 class Homepage extends Component {
   render() {
-    const { articles } = this.props;
+    const { articles, createNewArticle } = this.props;
     return (
       <div>
-        {articles.map(article => <p key={article.id}>{article.title}</p>)}
+        {articles.map(article => (
+          <p key={article.id}>{article.title}</p>
+        ))}
+        <form
+          action="submit"
+          name="article"
+          onSubmit={createNewArticle}
+        >
+          <input type="text" name="article" />
+          <button type="submit">Add Article</button>
+        </form>
       </div>
-    )
+    );
   }
 }
 
 const mapState = state => ({
   articles: state.article.all
-})
+});
 
 const mapDispatch = dispatch => ({
-  createNewArticle: (url) => dispatch(createNewArticle(url))
-})
+  createNewArticle: evt => {
+    const url = evt.target.article.value;
+    dispatch(createNewArticle(url));
+  }
+});
 
-export default connect(mapState, mapDispatch)(Homepage);
+export default connect(
+  mapState,
+  mapDispatch
+)(Homepage);
