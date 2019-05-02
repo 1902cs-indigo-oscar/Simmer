e6a50256e85342a3ff2e3c41d5c98456763a7d5b
Kenny Ye <kye6932@gmail.com>
2019-05-02 14:35:53 -0400

    Add backend to app

---
+++ server/api/articles.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const { Article } = require('../db/models');
+module.exports = router;
+
+router.get('/', async (req, res, next) => {
+  try {
+    console.log('Hit Me');
+    if (req.user) {
+      const articles = await Article.findAll({
+        where: { userId: req.user.id },
+      });
+      res.json(articles);
+    }
+    res.sendStatus(404);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    if (req.user) {
+      const newArticle = {
+        url: req.body.url,
+        site: req.body.site,
+        title: req.body.title,
+        author: req.body.author,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        misc: req.body.misc,
+        userId: req.user.id,
+      };
+      const createdArticle = await Article.create(newArticle);
+      res.json(createdArticle);
+    }
+    res.sendStatus(404);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:articleId', async (req, res, next) => {
+  try {
+    if (req.user) {
+      await Article.destroy({
+        where: {
+          id: req.params.articleId,
+          userId: req.user.id,
+        },
+      });
+      res.sendStatus(204);
+    }
+    res.sendStatus(404);
+  } catch (err) {
+    next(err);
+  }
+});
