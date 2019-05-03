fe9d46996b0a873624cbb6cd84018ef768e11d09
Brook Li <absreim@github.com>
2019-05-03 15:49:47 -0400

    Added conditional settings for CORS, reverse proxy, and a blank seed file.

---
+++ script/seed.js
@@ -0,0 +1,24 @@
+const db = require('../server/db')
+
+async function seed() {
+  await db.sync({force: true})
+  console.log('db synced!')
+}
+
+async function runSeed() {
+  console.log('seeding...')
+  try {
+    await seed()
+  } catch (err) {
+    console.error(err)
+    process.exitCode = 1
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+if (module === require.main) {
+  runSeed()
+}
