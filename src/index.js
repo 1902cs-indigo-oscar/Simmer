b06869bd72f7712a728baf0ce86bdf0d2719cc42
Brandon Rowe <brandonhrowe@gmail.com>
2019-05-06 16:27:54 -0400

    cleaned up bulma files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
-import './style/bulma/css/bulma.css'
+import './style/bulma.css'
 
 ReactDOM.render(
   <Provider store={store}>
