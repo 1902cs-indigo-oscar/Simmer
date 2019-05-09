a6bfcbc0fe61e47b982a7093a7552836723c81f5
Kenny Ye <kye6932@gmail.com>
2019-05-09 11:43:45 -0400

    Add guest user confirmation test

--- server/auth/auth.spec.js
+++ server/auth/auth.spec.js
@@ -1,54 +1,68 @@
 const request = require('supertest-session');
-const {expect} = require('chai')
+const { expect } = require('chai');
 
-const db = require("../db");
-const app = require("../")
+const db = require('../db');
+const app = require('../');
 
 describe('User created using the /signup route', () => {
+  let authenticatedSession = null;
 
-  let authenticatedSession = null
-
-  beforeEach("Returns a user created using the API", (done) => {
+  beforeEach('Returns a user created using the API', done => {
     db.sync({ force: true }).then(() => {
-      let session = request(app)
+      let session = request(app);
       session
-        .post("/auth/signup")
+        .post('/auth/signup')
         .send({
           firstName: 'John',
           lastName: 'Doe',
           email: 'johndoe@gmail.com',
-          password: '12345'
+          password: '12345',
         })
         .expect(200)
-        .end((err, res) => {
-          const resJson = JSON.parse(res.text)
-          if (err) {
-            return done(err);
-          }
-          expect(resJson.firstName).to.be.equal('John');
-          expect(resJson.lastName).to.be.equal('Doe');
-          expect(resJson.email).to.be.equal('johndoe@gmail.com');
-          authenticatedSession = session
+        .then(res => {
+          const resJSON = JSON.parse(res.text);
+          expect(resJSON.firstName).to.equal('John');
+          expect(resJSON.lastName).to.equal('Doe');
+          expect(resJSON.email).to.equal('johndoe@gmail.com');
+          authenticatedSession = session;
           return done();
         })
-    })
-  })
+        .catch(err => {
+          return done(err);
+        });
+    });
+  });
 
-  it("Returns a status indicating the user is logged in", (done) => {
+  it('Returns a status indicating the user is logged in', done => {
     authenticatedSession
-      .get("/auth/me")
+      .get('/auth/me')
+      .expect(200)
+      .then(res => {
+        const resJSON = JSON.parse(res.text);
+        expect(resJSON.firstName).to.equal('John');
+        expect(resJSON.lastName).to.equal('Doe');
+        expect(resJSON.email).to.equal('johndoe@gmail.com');
+        return done();
+      })
+      .catch(err => {
+        return done(err);
+      });
+  });
+});
+
+describe('A guest user', () => {
+  let guestSession = request(app);
+
+  it('should not have any indication of being logged in', done => {
+    guestSession
+      .get('/auth/me')
       .expect(200)
       .end((err, res) => {
-        const resJson = JSON.parse(res.text)
         if (err) {
           return done(err);
         }
-        expect(resJson.firstName).to.be.equal('John');
-        expect(resJson.lastName).to.be.equal('Doe');
-        expect(resJson.email).to.be.equal('johndoe@gmail.com');
+        expect(res.text).to.equal('');
         return done();
-      })
-  })
-
-})
-
+      });
+  });
+});
