fc4e1482ab61c40e956fb947bbcf622ca719cb6d
gwu358 <gwu358@gmail.com>
2019-05-08 22:49:13 -0400

    customize extension page and icon

--- extension/popup.js
+++ extension/popup.js
@@ -1,44 +1,46 @@
-// import allrecipesScraper from './scraping/all-recipes.js';
 import scraper from './scraping/index.js';
 
 const savePageButton = document.getElementById('save-page-btn')
 const pageSavedMessage = document.getElementById('page-saved-message')
 
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  console.log(tab);
+});
+
 checkLoginStatus();
 var url, site;
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    url = tabs[0].url;
-    const urlTail = url.split("www.")[1];
-    if (urlTail) site = urlTail.split(".com")[0];
+  url = tabs[0].url;
+  const urlTail = url.split("www.")[1];
+  if (urlTail) site = urlTail.split(".com")[0];
 })
 
 savePageButton.addEventListener('click', function (event) {
-    event.preventDefault()
-    scraper[site](url).then(
-        article => {
-            console.log(article);
-            fetch('https://simmer.brook.li/api/articles/scraped', {
-                method: 'POST',
-                mode: 'cors',
-                credentials: 'include',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(
-                    article
-                )
-            }).then(response => {
-                console.log(response)
-                if (response.status >= 400 && response.status < 500)
-                    throw new Error('either you haven\'t logged in or the page cannot be scraped')
-                else
-                pageSavedMessage.innerText = 'Page saved successfully!';
+  event.preventDefault()
+  scraper[site](url).then(
+    article => {
+      console.log(article);
+      fetch('https://simmer.brook.li/api/articles/scraped', {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+          article
+        )
+      }).then(response => {
+        if (response.status >= 400 && response.status < 500)
+          throw new Error('either you haven\'t logged in or the page cannot be scraped')
+        else
+          pageSavedMessage.innerText = 'Page saved successfully!';
 
-            }).catch(error => {
-                pageSavedMessage.innerText = 'Page saving failed: ' + error.message
-            })
-        }
-    );
+      }).catch(error => {
+        pageSavedMessage.innerText = 'Page saving failed: ' + error.message
+      })
+    }
+  );
 })
 
 const loginForm = document.getElementById('auth-form')
@@ -47,65 +49,63 @@ const passwordInput = document.getElementById('password')
 const loginErrorMessage = document.getElementById('login-error-message')
 
 loginForm.addEventListener('submit', function (event) {
-    event.preventDefault()
-    fetch('https://simmer.brook.li/auth/login', {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email: usernameInput.value,
-            password: passwordInput.value
-        })
-    }).then(response => {
-        if (response.status === 401) {
-            loginErrorMessage.innerText = 'wrong username and/or password';
-        }
-        else {
-            response.json().then(data => {
-                checkLoginStatus();
-            })
-        }
-    }).catch(error => {
-        loginErrorMessage.innerText = 'Login request failed: ' + error
+  event.preventDefault()
+  fetch('https://simmer.brook.li/auth/login', {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: usernameInput.value,
+      password: passwordInput.value
     })
-    loginForm.reset()
+  }).then(response => {
+    if (response.status === 401) {
+      loginErrorMessage.innerText = 'wrong username and/or password';
+    }
+    else {
+      checkLoginStatus();
+    }
+  }).catch(error => {
+    loginErrorMessage.innerText = 'Login request failed: ' + error
+  })
+  loginForm.reset()
 })
 
 const loginInfo = document.getElementById('login-info')
 
 function checkLoginStatus() {
-    fetch('https://simmer.brook.li/auth/me', {
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include'
-    }).then(response => {
-        if (response.status === 200) {
-            response.json().then(data => {
-                const logoutButton = document.createElement('button');
-                logoutButton.innerText = 'logout';
-                if (url && scraper[site] && url.includes('/recipe'))
-                    savePageButton.disabled = false;
-                logoutButton.onclick = function () {
-                    savePageButton.disabled = true;
-                    fetch('https://simmer.brook.li/auth/logout', {
-                        method: 'POST',
-                        mode: 'cors',
-                        credentials: 'include'
-                    }).then(() => {
-                        location.reload();
-                    }).catch(error => {
-                        console.error(error);
-                    })
-                }
-                loginInfo.innerHTML = '<p>Hello, ' + data.email + '<p>';
-                loginInfo.appendChild(logoutButton);
-            })
+  fetch('https://simmer.brook.li/auth/me', {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include'
+  }).then(response => {
+    if (response.status === 200) {
+      response.json().then(data => {
+        const logoutButton = document.createElement('button');
+        logoutButton.innerText = 'logout';
+        if (url && scraper[site] && url.includes('/recipe'))
+          savePageButton.disabled = false;
+        logoutButton.onclick = function () {
+          savePageButton.disabled = true;
+          fetch('https://simmer.brook.li/auth/logout', {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include'
+          }).then(() => {
+            location.reload();
+          }).catch(error => {
+            console.error(error);
+          })
         }
-        savePageButton.disabled = true;
-    }).catch(error => {
-        console.error(error);
-    })
+        loginInfo.innerHTML = '<p>Hello, ' + data.email + '<p>';
+        loginInfo.appendChild(logoutButton);
+      })
+    }
+    savePageButton.disabled = true;
+  }).catch(error => {
+    console.error(error);
+  })
 }
